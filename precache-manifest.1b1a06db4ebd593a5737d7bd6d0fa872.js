93acc0dfd6824ac39be82ba83cee0a15036dcd48
Elian Fusu <elian.fusu@barco.com>
2019-02-01 14:51:57 +0200

    Updates

---
+++ precache-manifest.1b1a06db4ebd593a5737d7bd6d0fa872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4d4395064f17508fdfe",
+    "url": "/react-crash-course/static/js/runtime~main.b4d43950.js"
+  },
+  {
+    "revision": "c474bbb43f1cdc9fbfd3",
+    "url": "/react-crash-course/static/js/main.c474bbb4.chunk.js"
+  },
+  {
+    "revision": "5849ec62572ef070ae9d",
+    "url": "/react-crash-course/static/js/1.5849ec62.chunk.js"
+  },
+  {
+    "revision": "c474bbb43f1cdc9fbfd3",
+    "url": "/react-crash-course/static/css/main.7a0f24f6.chunk.css"
+  },
+  {
+    "revision": "14ef63b95f23abe0747dbc3cd0ba5eac",
+    "url": "/react-crash-course/index.html"
+  }
+];
